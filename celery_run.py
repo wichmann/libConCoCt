ca523dc7e87045f6b3ab3c78ec2484e11b6fb843
Christian Wichmann <cw1981@gmx.de>
2015-07-30 09:09:49 +0200

    Added Celery wrapper for libConCoCt.

--- celery_run.py
+++ celery_run.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
 
 import celery_tasks
 import time
 import os
 from libConCoCt import Task, Solution
 
-
-building = celery_tasks.build_and_check_task_with_solution.delay(os.path.join('..', 'private', 'tasks', 'task2'), '')
+task_directory = os.path.join('tasks', 'greaterZero')
+solution_file = (os.path.join('solutions', 'greaterZero', 'user2', 'solution.c'), )
+building = celery_tasks.build_and_check_task_with_solution.delay(task_directory,
+                                                                 solution_file)
 
 while not building.ready():
     print('Waiting...')
-    time.sleep(1)
+    time.sleep(0.2)
 
 print(building.get())
--- celery_tasks.py
+++ celery_tasks.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 
 """
 Implements a celery worker function to build a project and unit test it.
@@ -16,6 +17,7 @@ Stop all workers with:
 """
 
 
+import sys
 from celery import Celery
 from libConCoCt import Task, Solution, ConCoCt
 
@@ -34,16 +36,27 @@ app = Celery('tasks', backend=BACKEND, broker=BROKER_URL)
 # TODO Remove name parameter here and cleanup imports in Celery worker (this
 #      file) and 'entry' controller.
 @app.task(name='applications.ConCoct.modules.celery_tasks.build_and_check_task_with_solution')
-def build_and_check_task_with_solution(tasks_store_path, solution_path):
+def build_and_check_task_with_solution(task_store_path, solution_file_list):
+    """
+    Builds a given task with a given solution by a user. The task defines unit
+    tests and helper function that are used to determine, if the task has been
+    sucessfully solved.
+
+    :param task_store_path: path to the task directory containing the task
+                            description, configuration file and all source
+                            files necessary to build and test the task
+    :param solution_file_list: list of files submitted as possible solution for
+                               the given task
+    """
     try:
-        t = Task(tasks_store_path)
+        t = Task(task_store_path)
     except FileNotFoundError as e:
         sys.exit(e)
-    s = Solution(t) #, 'path to solution'
+    s = Solution(t, solution_file_list)
     try:
         w = ConCoCt()
     except FileNotFoundError as e:
         sys.exit(e)
-    p = t.get_test_project(None) # s
+    p = t.get_test_project(s)
     r = w.check_project(p)
     return r.to_json()
